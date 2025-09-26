609f3a98d8088275c5dc32d531568b1349b2d5cd
jeferson26 <jefersonmauropairasanchez6@gmail.com>
2025-09-26 01:12:10 -0500

    first commit

---
+++ ejercicio01/ejercicio1.py
@@ -0,0 +1,10 @@
+base = int(input("Ingrese la base del triangulo: "))
+altura = int(input("Ingrese la altura del triangulo: "))
+
+area = base * altura / 2
+
+print(f"El area del triangulo es: {area}")
+
+
+
+
